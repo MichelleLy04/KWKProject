879e16173a3df72efdbb04ebddd7d321e9261783
MICHELLE LY <michellely04@icloud.com>
2020-07-29 19:08:57 -0700

    ashanti wants this

--- KWKAPP/ViewController.swift
+++ KWKAPP/ViewController.swift
@@ -1,20 +1,49 @@
-//
-//  ViewController.swift
-//  KWKAPP
-//
-//  Created by Ava Deguzman on 7/28/20.
-//  Copyright © 2020 Ava Deguzman. All rights reserved.
-//
 
-import UIKit
+import SwiftUI
 
-class ViewController: UIViewController {
+class SecondViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    @IBOutlet weak var label: UILabel!
+
+
+    @IBAction func slider(_ sender: UISlider) {
+
+         let currentValue = Int(sender.value)
+
+        label.text = "\(currentValue)"}
+
+        struct ContentView: View {
+        
+        @State var mood: Double = 0.0
+        
+    var body: some View {
+        VStack {
+            Slider(value: $mood, in: -50...50, step: 25)
+
+            if mood == (Double) {
+                if mood == (50) {print ("😃") } else if mood == (25) { print ("🙂")}
+                else if mood == (0) {print ("😐") }; if mood == (-25) { print ("😕") } else if mood == (-50) {
+                print ("😔")
+        }
     }
-
-
-}
-
+        }
+            }
+            
+//import UIKit
+//
+//class SecondViewController: UIViewController {
+//
+//    @IBAction func linkClicked(sender: AnyObject) {
+//        openUrl(urlStr: "https://news.yale.edu/2020/07/27/improve-students-mental-health-yale-study-finds-teach-them-breathe")
+//    }
+//
+//    @IBAction func linkClicked2(_ sender: Any) {
+//    }
+//    func openUrl(urlStr:String!) {
+//
+//         if let url = NSURL(string:urlStr) {
+//            UIApplication.shared.open(url as URL)
+//    }
+//}
+//}
